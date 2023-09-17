0dbce20e996aa19d2bbb7b49e9ba062e4e23f391
Vaishnavi Chilakamarthi <aruna.avvs@gmail.com>
2023-09-16 23:07:55 -0400

    fr

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,3 +4,6 @@ class JobRoleForm(forms.Form):
     role_name = forms.CharField(label='Job Role Name', max_length=255)
     
 
+class TextInputForm(forms.Form):
+    text = forms.CharField(max_length=128)
+    role_company=forms.CharField(max_length=128, required=False)
